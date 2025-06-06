5036840f5520fb5f7fa98720ba8698617758705e
sumam1999 <sumam19992025@gmail.com>
2025-06-06 10:51:27 +0530

    Total_&_AverageMarks_Program

---
+++ Total_Average_Program.java
@@ -0,0 +1,15 @@
+package JavaBasic;
+
+
+public class Total_Average_Program {
+	public static void main(String[] args) {
+	
+	int Math = 63, Science = 75 , English = 98 ;
+	int totalMarks = Math +Science + English ;
+	System.out.println("Total Marks of 3 Subjects is "+ totalMarks);
+	int average = totalMarks /3;
+	System.out.println("Average marks of 3 subjects is"+ average);
+
+	}
+
+}
