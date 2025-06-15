f11301771495e7a1d48ee08687410134327ce770
sumam1999 <sumam19992025@gmail.com>
2025-06-15 17:40:49 +0530

    Add files via upload

---
+++ MultiplicationProgram.java
@@ -0,0 +1,18 @@
+package JavaBasic;
+
+public class MultiplicationProgram {
+
+	public static void main(String[] args) {
+		
+		        for (int i = 1; i <= 5; i++) {
+		            System.out.println("Multiplication Table of " + i + ":");
+		            for (int j = 1; j <= 10; j++) {
+		                System.out.println(i + " * " + j + " = " + (i * j));
+		            }
+		            System.out.println(); // Add a blank line between tables
+		        }
+		    }
+		
+	}
+
+
---
+++ NumbersDivisible.java
@@ -0,0 +1,17 @@
+package JavaBasic;
+
+public class NumbersDivisible {
+
+	public static void main(String[] args) {
+		
+		
+		        for (int i = 1; i <= 100; i++) {
+		            if (i % 3 == 0 && i % 5 == 0) {
+		                System.out.println(i);
+		            }
+		        }
+		    }
+		
+	}
+
+
